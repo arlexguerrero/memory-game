57faa4a8edcdca25d91dfd4e4459495f44072fcf
Sergio Guerrero <guerrerovillasergio@gmail.com>
2022-10-10 16:21:33 -0500

    active todos los emojis

--- js/variables-globales.js
+++ js/variables-globales.js
@@ -1,7 +1,7 @@
 var movimientos = 0;
 
-var grupoTarjetas = ["🦄", "🤡"];
+var grupoTarjetas = ["🦄", "🤡", "🤬", "🥵", "🤖", "🎃", "🐸"];
 
 var totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
 
-//, "🤬", "🥵", "🤖", "🎃", "🐸"
+//
